e217a09c8f69ee206c40db547edaffa45ce8e95f
AKA <c.oka@free.fr>
2025-02-20 17:45:42 +0100

    bouton de remonté

--- src/index.js
+++ src/index.js
@@ -254,3 +254,24 @@ document.addEventListener("DOMContentLoaded", function () {
     ease: "power2.out",
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  // Vérifier le scroll et afficher/cacher le bouton
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      scrollTopBtn.classList.add("show");
+    } else {
+      scrollTopBtn.classList.remove("show");
+    }
+  });
+
+  // Remonter en haut en cliquant sur le bouton
+  scrollTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
